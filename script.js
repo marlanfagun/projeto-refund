bb77f76b3de181bfe8a338b9fa44e2d879cf2072
Marlan Fagundes <marlanetci@gmail.com>
2024-08-05 22:12:17 -0300

    project conclude

--- script.js
+++ script.js
@@ -94,6 +94,9 @@ function expenseAdd(newExpense) {
     //Adiciona o item na lista
     expenseList.append(expenseItem)
 
+    //Limpa os inputs para adicionar outro item
+    formClear()
+
     //Atualiza os totais
     updateTotals()
   } catch (error) {
@@ -150,4 +153,30 @@ function updateTotals() {
     console.log(error)
     alert("Não foi possível atualizar os totais.")
   }
+}
+
+//Evento que captura o clique nos itens da lista
+expenseList.addEventListener("click", function(event){
+  //Verifica se o elemento clicado é o ícone de remover
+  if(event.target.classList.contains("remove-icon")) {
+    //Obtém a li pai do elemento clicado
+    const item = event.target.closest(".expense")
+
+    //Remove item da lista
+    item.remove()  
+  }
+
+  //Atualiza os totais
+  updateTotals()
+
+}) 
+
+function formClear() {
+  //Limpa os inputs
+  expense.value = ""
+  category.value = ""
+  amount.value = ""
+
+  //Coloca o foco no input de despesa
+  expense.focus()
 }
